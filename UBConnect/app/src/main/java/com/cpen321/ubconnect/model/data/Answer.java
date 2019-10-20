4a2c3e638d74b0dc1203490d65ecef31a228ca4b
samiri98 <soroushamirizadeh@gmail.com>
2019-10-20 13:39:19 -0700

    deleting front-end

--- UBConnect/app/src/main/java/com/cpen321/ubconnect/model/data/Answer.java
+++
@@ -1,5 +0,0 @@
-package com.cpen321.ubconnect.model.data;
-
-public class Answer {
-
-}
--- UBConnect/app/src/main/java/com/cpen321/ubconnect/ui/PostQuestionActivity.java
+++
@@ -1,6 +0,0 @@
-package com.cpen321.ubconnect.ui;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PostQuestionActivity extends AppCompatActivity {
-}
