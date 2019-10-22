9dfa24de8e524970f6e0fb6044150cf12e26ebbe
john <johnmarquez12@github.com>
2019-10-22 14:21:08 -0700

    updated question route

--- rest-server/utils/validator.js
+++ rest-server/utils/validator.js
@@ -1,18 +1,20 @@
-const {body} = require('express-validator');
+const {checkBody} = require('express-validator');
 const User = require('../schema/user');
 
 const userValidator = body('owner')
     .custom(async (value) =>
     {
+        console.log('user id is :' + value);
 
-        console.log(value);
         try {
             let user = await User.findById(value);
-
-        if (!user)
-            return Promise.reject('User Does Not Exist');
+            if (!user)
+                return Promise.reject('User Does Not Exist');
+            else
+                return true; 
         } catch (err) {
-            next(err);
+            console.error(err);
+            return Promise.reject(err);
         }
         
     });
