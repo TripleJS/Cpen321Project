2da4553b24ec116ec63020daf000077cfabfb10f
john <johnmarquez12@github.com>
2019-10-23 00:36:24 -0700

    added some socket stuff

--- socket-server/socket.js
+++ socket-server/socket.js
@@ -1,5 +1,14 @@
 const socketio = require('socket.io');
- 
+const admin = require("firebase-admin");
+const serviceAccount = require("../fcm/ubconnect-ec25e-firebase-adminsdk-4zww0-0d250bdc3b.json");
+const Question = require('../rest-server/schema/questions');
+const User = require('../rest-server/schema/user');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ubconnect-ec25e.firebaseio.com"
+});
+
 class SocketServer {
     
     constructor(server) {
@@ -9,13 +18,56 @@ class SocketServer {
     startServer() {
         this.io.on('connection', (socket) => {
 
-            socket.on('join', (userNickname) => {
-                console.log(userNickname + ": joined chat");
+            socket.on('join', async (userId, questionId) => {
+                console.log(userId + ": joined chat and current question id is " + questionId);
+                
+                let fcmAccessToken; 
 
-                socket.broadcast.emit('userjoinedthechat', userNickname + " has joined the chat");
+                try {
+                    let questionData = await Question.findById(questionId);
+                    console.log('Question Data: ' + questionData);
+
+                    const questionOwner = questionData.owner;
+
+                    let user = await User.findById(questionOwner);
+                    console.log('User is: ' + user);
+
+                    fcmAccessToken = user.fcmAccessToken;
+                    
+                    admin.messaging().subscribeToTopic(fcmAccessToken, questionId)
+                        .then((res) => {
+                            console.log('subscribed to topic ' + res);
+                        })
+                        .catch((err) => {
+                            console.err('error subscribing ' + err);
+                        });
+
+                } catch (error) {
+                    console.error(error);
+                }
+
+                const message = {
+                    data : {
+                        message : 'Some user is answering your question'
+                    },
+                    token: fcmAccessToken
+                  };
+                  
+                  // Send a message to the device corresponding to the provided
+                  // registration token.
+                admin.messaging().send(message)
+                    .then((response) => {
+                      // Response is a message ID string.
+                      console.log('Successfully sent message:', response);
+                    })
+                    .catch((error) => {
+                      console.log('Error sending message:', error);
+                    });
+
+                socket.broadcast.emit('userjoinedthechat', userId + " has joined the chat");
             });
 
-            socket.on('messagedetection', (senderNickname, messageContent) => {
+            socket.on('messagedetection', (senderNickname, messageContent, ) => {
 
                 console.log(senderNickname + " sent " + messageContent);
 
