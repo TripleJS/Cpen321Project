6f7a8fd428b640ef60e5bc471f010b9ac4233ea0
johnmarquez12 <ajxz12@gmail.com>
2019-11-05 11:59:22 -0800

    fixed issues related to complex logic

--- rest-server/utils/suggestions/cosineSimilarity.js
+++ rest-server/utils/suggestions/cosineSimilarity.js
@@ -3,6 +3,11 @@ const getCosineSimilarity = (v1, v2) => {
 
     let sumxx = 0; let sumyy = 0; let sumxy = 0;
     if(v1.length < v2.length) {
+
+        for (let j = 0; j < (v2.length - v1.length); j++) {
+            v1.push(0);
+        }
+
         let i;
         for (i of v1) {
             let x = v1[i]; 
@@ -14,6 +19,11 @@ const getCosineSimilarity = (v1, v2) => {
             
     } else {
         let i;
+
+        for (let j = 0; j < (v1.length - v2.length); j++) {
+            v1.push(0);
+        }
+
         for(i of v2) {
             let x = v1[i]; 
             let y = v2[i];
@@ -35,10 +45,13 @@ const getBagOfQuestions = (questionKeywords, question) => {
         if (cosineSimilarity > 0.4) {
             bagOfQuestions.push(questionKeywords[i]);
         }
+
         if (bagOfQuestions > 4) {
             return bagOfQuestions;
         }
     }
+
+    return bagOfQuestions;
 };
 
 module.exports = getBagOfQuestions;
