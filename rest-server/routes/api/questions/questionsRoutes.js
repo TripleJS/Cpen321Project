34d20e058d75743f542ba22f04d86fa7b5cc1d96
john <johnmarquez12@github.com>
2019-11-17 13:58:34 -0800

    removed param from search

--- rest-server/routes/api/questions/questionsRoutes.js
+++ rest-server/routes/api/questions/questionsRoutes.js
@@ -8,7 +8,7 @@ const passportConfig = require("../../../middleware/passport");
 router.get("/questions/get-question/:questionId", questionController.getQuestion);
 router.post("/questions/post-question", userValidator, questionController.postQuestion);
 router.get("/questions/suggest/:userId", passport.authenticate("jwt", {session: false}), questionController.suggestedQuestions);
-router.get("/search/:searchQuery", questionController.searchQuestion);
+router.get("/search", questionController.searchQuestion);
 router.post("/questions/swipe", questionController.swipedQuestion);
 router.get("/questions/recent/:userId", questionController.getMostRecentQuestion);
 
