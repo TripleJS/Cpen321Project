cc09203854d87cce614297c6cf5426559764778f
john <johnmarquez12@github.com>
2019-11-17 19:17:09 -0800

    some push

--- rest-server/controller/answer.js
+++ rest-server/controller/answer.js
@@ -1,12 +1,14 @@
 const Answer = require("../schema/answers");
 const Question = require("../schema/questions");
 const {errorCatch} = require("../utils/errorHandler");
+const {logger} = require("../../logger");
 
 const getMostRecentAnswerId = async (req, res, next) => {
     const userId = req.params.userId;
 
     try {
         const latestAnswer = await Answer.findOne({userAnswerID : userId}).sort({date : -1});
+        logger.info(latestAnswer);
         const relatedQuestionId = latestAnswer.questionRef;
 
         const relatedQuestion = await Question.findById(relatedQuestionId);
