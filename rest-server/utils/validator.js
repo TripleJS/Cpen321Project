5ec6e699b4d21da32ddd1e852ed1b6cf9a56ab76
john <johnmarquez12@github.com>
2019-11-17 17:31:15 -0800

    Fixed some errors

--- rest-server/utils/validator.js
+++ rest-server/utils/validator.js
@@ -2,12 +2,12 @@ const {body} = require("express-validator");
 const User = require("../schema/user");
 const {logger} = require("../../logger");
 
-const userValidator = body("email")
+const userValidator = body("userId")
     .custom(async (value) => {
         logger.info("user id is :" + value);
 
         try {
-            let user = await User.find({email: email});
+            let user = await User.findById(userId);
             
             if (!user) {
                 return Promise.reject("User Does Not Exist");
