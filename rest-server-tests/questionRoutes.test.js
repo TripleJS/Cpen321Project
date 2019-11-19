d5c1cd0fc692d71345aa45033533c63e750282d5
john <johnmarquez12@github.com>
2019-11-19 14:18:06 -0800

    updated tests for backend

--- rest-server-tests/questionRoutes.test.js
+++ rest-server-tests/questionRoutes.test.js
@@ -1,6 +1,27 @@
 const questionController = require("../rest-server/controller/question");
 const errorHandler = require("../rest-server/utils/errorHandler");
 
+const {MongoClient} = require('mongodb');
+ 
+describe('insert', () => {
+  let connection;
+  let db;
+ 
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db();
+  });
+ 
+  afterAll(async () => {
+    await connection.close();
+  });
+
+});
+
+
 
 describe("", async () => {
 
--- rest-server-tests/userRoutes.test.js
+++ rest-server-tests/userRoutes.test.js
@@ -1,81 +1,140 @@
 const userController = require("../rest-server/controller/user");
-import * as errorHandler from "../rest-server/utils/errorHandler";
+const errorHandler = require("../rest-server/utils/errorHandler");
+const {MongoClient} = require('mongodb');
 
-describe("Check Login", async () => {
-    const errorCatchMock = jest.spyOn(errorHandler, "errorCatch");
-    const errorThrowMock = jest.spyOn(errorHandler, "errorThrow");
+describe("User Route Test Suite", () => {
+  let connection;
+  let db;
 
-    test("Logging In With Email", async () => {
-        
+  const errorCatchMock = jest.spyOn(errorHandler, "errorCatch");
+  const errorThrowMock = jest.spyOn(errorHandler, "errorThrow");
+                  
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
+    db = await connection.db();
+  });
+  
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    test("Logging in With ", async() => {
+  describe("Testing Signup", () => {
 
-    });
+    const users = db.collection('users');
 
-    test("Logging in With Email where User Exists", async () => {
+    const mockRequest = (email, username, password) => {
+      const req = {};
+      req.email = jest.fn().mockReturnValue(req);
+      req.username = jest.fn().mockReturnValue(req);
+      req.password = jest.fn().mockReturnValue(req);
 
-    });
+      return req;
+    };
 
-    test("Logging in With Facebook", async () => {
+    const mockResponse = () => {
+      const res = {};
+      res.status = jes.fn().mockReturnValue(res);
+      res.user = jest.fn().mockReturnValue(res);
+      res.jwt = jest.fn().mockReturnValue(res);
+      return res;
+    };
 
-    });
-});
+    const next = () => {
 
+    };
 
-describe("Testing Signup", async () => {
     test("Signing Up Correctly", async () => {
-
+      const req = mockRequest("someemail@email.com", "someusername", "password");
+      const res = mockResponse();
+      await userController.addUser(req, res, next);
+      expect(res.status).toHaveBeenCalledWith(201);
     });
 
     test("Signing Up with Email that Exists", async () => {
 
     });
-});
 
-describe("Testing Rate", async () => {
-    test("Rating With Correct Inputs", async () => {
+  });
 
-    });
+  describe("Check Login", () => {
+      const mockResponse = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
 
-    test("Rating a user that does not exist", async() => {
+      const mockRequest = () => {
+        const res = {};
+        res.user = jest.fn().mockReturnValue(res);
+        res.jwt = jest.fn().mockReturnValue(res);
+        return res;
+      };
 
-    });
+      test("Logging In With Email", async () => {
+          
+      });
 
-    test("Rating a user with a user that does not exist", async() => {
+      test("Logging in With ", async() => {
 
-    });
+      });
 
+      test("Logging in With Email where User Exists", async () => {
+      });
 
-    test("Rating a user twice with same inputs", async () => {
+      test("Logging in With Facebook", async () => {
 
-    });
+      });
+  });
 
-    test("Rating a user twice with different inputs", async() => {
+  describe("Testing Rate",  () => {
+      test("Rating With Correct Inputs", async () => {
 
-    });
+      });
 
-}); 
+      test("Rating a user that does not exist", async() => {
 
-describe("Testing Report", async () => {
-    test("Reporting With Correct Inputs", async () => {
+      });
 
-    });
+      test("Rating a user with a user that does not exist", async() => {
 
-    test("Reporting a user that does not exist", async() => {
-
-    });
-
-    test("Reporting a user with a user that does not exist", async() => {
-
-    });
-
-    test("Reporting a user twice", async () => {
-
-    });
+      });
 
 
-});
+      test("Rating a user twice with same inputs", async () => {
+
+      });
+
+      test("Rating a user twice with different inputs", async() => {
+
+      });
+
+  }); 
+
+  describe("Testing Report",  () => {
+      test("Reporting With Correct Inputs", async () => {
+
+      });
+
+      test("Reporting a user that does not exist", async() => {
+
+      });
+
+      test("Reporting a user with a user that does not exist", async() => {
+
+      });
+
+      test("Reporting a user twice", async () => {
+
+      });
+
+
+  });
+})
+
 
 
 
