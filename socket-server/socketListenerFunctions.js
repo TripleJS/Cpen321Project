6555621b8a7c0f82b8a97bff4c3bc5df4aaaf4c3
john <johnmarquez12@github.com>
2019-11-23 19:05:38 -0800

    updated search

--- socket-server/socketListenerFunctions.js
+++ socket-server/socketListenerFunctions.js
@@ -8,17 +8,17 @@ const onJoin = async (userId, questionId) => {
     let questionData;
 
     try {
-        // logger.info(userId);
+        logger.info(userId);
         questionData = await Question.findById(questionId);
         logger.info("Question Data: " + questionData);
 
-        // const questionOwner = questionData.owner;
-        // let user = await User.findById(questionOwner);
-        // logger.info("User is: " + user);
+        const questionOwner = questionData.owner;
+        let user = await User.findById(questionOwner);
+        logger.info("User is: " + user);
 
-        // fcmAccessToken = user.fcmAccessToken;
+        fcmAccessToken = user.fcmAccessToken;
 
-        // await subscribeToTopic(questionId, fcmAccessToken);
+        await subscribeToTopic(questionId, fcmAccessToken);
 
         const notification = createNotificationMessage("Your Question " + questionData.title + " is being answered", "Someone is answering your Question!");
         const inputData = {
